1d660fedb31b73602a403e5a9e8192b9657066f8
谷崎公紀 <marutto8@gmail.com>
2020-03-11 13:09:29 +0900

    アプリ完成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 50 }
-  validates :body, presence: true, length: { maximum: 2000 }
+  validates :title, presence: true
+  validates :body, presence: true
 end
